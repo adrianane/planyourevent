b46eca5bbcd166f0e6c51e678e380eafc8956ad3
Adriana Negrea <adriana.negrea@target-e.com>
2018-11-18 14:57:57 +0200

    [TASK] include navbar suing partials

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -25,16 +25,9 @@
                     @endauth
                 </div>
             @endif
-
+            @include('partials.navbar')
             <div class="content">
                 @yield('content')
-                <div class="links">
-                    <a href="/">Home</a>
-                    <a href="/foto/">Foto</a>
-                    <a href="/video/">Video</a>
-                    <a href="/locations/">Locations</a>
-                    <a href="/dashboard">My dashboard</a>
-                </div>
             </div>
         </div>
     </body>
